4450e6aa697d105b5069a40734b2eaf5018ce676
TripperW <86893841+TripperW@users.noreply.github.com>
2022-09-13 18:12:03 -0500

    Final version 1, need CSS 2 and implement swap

---
+++ function.js
@@ -0,0 +1,8 @@
+function style1()
+{
+    document.getElementById('page_style').setAttribute("href", "style1.css");
+}
+function style2()
+{
+    document.getElementById('page_style').setAttribute("href", "style2.css");
+}
