f6e9df0ee15e1f039d5f759fc571e70b77d5088d
TripperW <86893841+TripperW@users.noreply.github.com>
2022-09-13 21:41:20 -0500

    Final Version
    reduced to single style change button, changes persist

--- function.js
+++ function.js
@@ -1,25 +1,23 @@
 
 document.addEventListener("DOMContentLoaded", checkstyle());
-function checkstyle(){
+function checkstyle(currStyle){
     if(localStorage.getItem("style")=="style2.css"){
-        //change the css
-        document.getElementById('page_style').setAttribute("href", "style2.css");
-      } else if(localStorage.getItem("style")!="style1.css"){
-        localStorage.setAttribute("style", "style1.css");
+       // change the css
+       document.getElementById('page_style').setAttribute("href", "style2.css");
       }
 };
-
 function style1()
 {
-    if(localStorage.getItem("style")=="style1.css"){
-        //change the css
-        localStorage.setAttribute("style", "style2.css");
-        document.getElementById('page_style').setAttribute("href", "style2.css");
-      }
-     if(localStorage.getItem("style")=="style2.css"){
-        localStorage.setAttribute("style", "style1.css");
-        document.getElementById('page_style').setAttribute("href", "style1.css");
-      }
-    
-    
+    if(localStorage.getItem("style")=="style2.css"){
+    localStorage.clear();
+    document.getElementById('page_style').setAttribute("href", "style1.css");
+    } else{
+    localStorage.setItem("style","style2.css")
+    document.getElementById('page_style').setAttribute("href", "style2.css");
+    }
+}
+function style2()
+{
+    localStorage.setItem("style","style2.css")
+    document.getElementById('page_style').setAttribute("href", "style2.css");
 }
