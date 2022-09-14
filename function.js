282734add8c16779148cc86532b675e94ce7f7bf
TripperW <86893841+TripperW@users.noreply.github.com>
2022-09-13 21:12:37 -0500

    persisting CSS changes, need to fix button

--- function.js
+++ function.js
@@ -1,8 +1,25 @@
+
+document.addEventListener("DOMContentLoaded", checkstyle());
+function checkstyle(){
+    if(localStorage.getItem("style")=="style2.css"){
+        //change the css
+        document.getElementById('page_style').setAttribute("href", "style2.css");
+      } else if(localStorage.getItem("style")!="style1.css"){
+        localStorage.setAttribute("style", "style1.css");
+      }
+};
+
 function style1()
 {
-    document.getElementById('page_style').setAttribute("href", "style1.css");
-}
-function style2()
-{
-    document.getElementById('page_style').setAttribute("href", "style2.css");
+    if(localStorage.getItem("style")=="style1.css"){
+        //change the css
+        localStorage.setAttribute("style", "style2.css");
+        document.getElementById('page_style').setAttribute("href", "style2.css");
+      }
+     if(localStorage.getItem("style")=="style2.css"){
+        localStorage.setAttribute("style", "style1.css");
+        document.getElementById('page_style').setAttribute("href", "style1.css");
+      }
+    
+    
 }
